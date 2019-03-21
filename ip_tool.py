a3d9dba0597a6858fb06cbf7482cb903feecb178
hozfia <48630338+hozfia@users.noreply.github.com>
2019-03-22 00:31:13 +0200

    Update ip_tool.py

--- ip_tool.py
+++ ip_tool.py
@@ -1,11 +1,8 @@
 class ip_tool (object) :
-    a_digit = []
-    b_digit = []
-    c_digit = []
-    d_digit = []
+    
     def __init__(self, ip_add):
         self.ip_add =  ip_add 
-    def ip_digit (self) :
+    def ip_digit (self , digit_name) :
         dot_pos_index = []
         cou = 0 
         for i in self.ip_add :
@@ -23,12 +20,19 @@ class ip_tool (object) :
         for x in range(dot_pos_index[1] , dot_pos_index[2] - 1):
             c_digit.append(self.ip_add[x])    
         for x in range(dot_pos_index[2] , cou):
-            d_digit.append(self.ip_add[x])    
-        global a_digit
+            d_digit.append(self.ip_add[x])             
         a_digit = self.convert (a_digit)
         b_digit = self.convert (b_digit) 
         c_digit = self.convert (c_digit) 
         d_digit = self.convert (d_digit)
+        if digit_name == 1 :
+            return a_digit
+        if digit_name == 2 :
+            return b_digit
+        if digit_name == 3 :
+            return c_digit
+        if digit_name == 4 :
+            return d_digit
         
     def convert (self , list) :
         res = int("".join(map(str,list)))
@@ -37,7 +41,7 @@ class ip_tool (object) :
 
 #####################################    
 ip1 = ip_tool("323.23.21.3")
-#print(ip1.ip_digit.a_digit)
+print(ip1.ip_digit(4))
 
 				
 				
